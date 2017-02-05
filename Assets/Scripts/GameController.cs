ffc59790fee0cbbc15f83ada08f8380094928d58
James Sandford <james.sandford@blinkbyte.co.uk>
2017-02-05 11:06:33 +0000

    moved gameStart to its own method that is run with events

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,10 +5,13 @@ using UnityEngine.Networking;
 using System.Linq;
 namespace CardGame
 {
-    public class GameController : NetworkBehaviour {
+    public delegate void PlayerEvent();
+    public delegate void CardGameEvent();
+    
+        public class GameController : NetworkBehaviour {
+        public GameObject GameRules;
         public List<Player> players;
         public List<Deck> decks;
-        public int startingHandSize = 7;
         public int PlayerPriority;
         public Phases CurrentPhase;
         public int TurnNumber;
@@ -16,35 +19,30 @@ namespace CardGame
         public int PlayersTurn
         {
             get{
-                return TurnNumber % players.Count;
+                return TurnNumber % (players.Count +1);
             }
         }
         public static GameController Inst;
+
+        public event CardGameEvent GameStart;
+        
+       
         public void Start()
         {
             GameSetUp = false;
             Inst = this;
+            Instantiate(GameRules);
         }
         public void StartGame()
         {
             if (GameSetUp)
                 return;
+            GameStart();
             GameSetUp = true;
-            foreach( var temp in GameObject.FindGameObjectsWithTag("Player"))
-            {
-                players.Add(temp.GetComponent<Player>());
-            }
-            for(int i = 0;i< players.Count;i++)
-            {
-                players[i].playerNumber = i;
-                StartCoroutine(SetUpPlayer(i));
-               // players[i].StartGame();
-            }
         }
         private void Update()
         {
-            for(int pNumber = 0; pNumber < players.Count;pNumber++)
-                players[pNumber].CardsLoaded = decks[pNumber].CardsLoaded;
+            
         }
         public void drawACard(int playerNumer)
         {
@@ -69,24 +67,14 @@ namespace CardGame
         {
             if (PlayerNumber != PlayerPriority)
                 return;
-            //PlayerPriority++;
-            if (PlayerPriority +1 >= players.Count)
+            PlayerPriority++;
+            if (PlayerPriority >= players.Count)
+                PlayerPriority = 0;
+            if(PlayerPriority == PlayersTurn)
                 NextPhase();
-            else
-                PlayerPriority++;
+           
         }
 
-        IEnumerator SetUpPlayer(int pNumber)
-        {
-            decks[pNumber].LoadCards();
-            yield return new WaitUntil(() => decks[pNumber].deckLoaded);
-            players[pNumber].CardsLoaded = decks[pNumber].CardsLoaded;
-            decks[pNumber].ShuffleCards();
-            for(int i = 0; i < startingHandSize; i++)
-            {
-                drawACard(pNumber);
-            }
-        }
 
     }
 }
---
+++ Assets/Scripts/MTG/GameStart.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CardGame.MTG
+{
+    public class GameStart : MonoBehaviour
+    {
+        GameController cont;
+        public int startingHandSize = 7;
+        public void Start()
+        {
+            cont = GameController.Inst;
+            cont.GameStart += new CardGameEvent(OnStart);
+        }
+        public void OnStart()
+        {
+            Debug.Log("GameStart");
+            foreach (var temp in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                cont.players.Add(temp.GetComponent<Player>());
+            }
+            for (int i = 0; i < cont.players.Count; i++)
+            {
+                cont.players[i].playerNumber = i;
+                StartCoroutine(SetUpPlayer(i));
+            }
+
+        }
+        public void Update()
+        {
+            for (int pNumber = 0; pNumber < cont.players.Count; pNumber++)
+                cont.players[pNumber].CardsLoaded = cont.decks[pNumber].CardsLoaded;
+        }
+        IEnumerator SetUpPlayer(int pNumber)
+        {
+            cont.decks[pNumber].LoadCards();
+            yield return new WaitUntil(() => cont.decks[pNumber].deckLoaded);
+            cont.players[pNumber].CardsLoaded = cont.decks[pNumber].CardsLoaded;
+            cont.decks[pNumber].ShuffleCards();
+            for (int i = 0; i < startingHandSize; i++)
+            {
+                cont.drawACard(pNumber);
+            }
+        }
+    }
+}
